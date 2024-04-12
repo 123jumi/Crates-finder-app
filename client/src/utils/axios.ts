1c4adc0961597a90cb14bb336ef85e88780b410d
Julien Minville <minville.julien1@gmail.com>
2024-04-11 20:01:11 -0400

    Update axios base URL to use environment variable

--- client/src/utils/axios.ts
+++ client/src/utils/axios.ts
@@ -1,7 +1,9 @@
 import axios from 'axios'
 
 
-export const http = axios.create({ baseURL: "https://www.cratesfinder.com/api" });
+export const http = axios.create({
+	baseURL: import.meta.env.VITE_API_URL,
+});
 
 http.interceptors.request.use(async (request) => {
 
