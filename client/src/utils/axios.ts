a6f85b9676e73f8e01c78c9ff26c6d176a1e2ca7
Julien Minville <minville.julien1@gmail.com>
2024-04-11 20:20:15 -0400

    Update axios base URL to use new API endpoint

--- client/src/utils/axios.ts
+++ client/src/utils/axios.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 
 export const http = axios.create({
-	baseURL: import.meta.env.VITE_API_URL,
+	baseURL: "https://crates-finder-5b6071ee0f7e.herokuapp.com/api",
 });
 
 http.interceptors.request.use(async (request) => {
